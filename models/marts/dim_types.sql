58fad9eb93893a4ddb9e9bcd1cf2380a2870b075
stef l <stepfano7@gmail.com>
2024-07-14 16:10:30 +0000

    dim types change

--- models/marts/dim_types.sql
+++ models/marts/dim_types.sql
@@ -1,11 +1,11 @@
 WITH raw_pokemon_reduced AS (SELECT * FROM {{ref('stg_dbtprojectpokemon__raw_pokemon')}}),
 
 type1table AS (SELECT
-pokedex_number, type1
+pokedex_number, type1 as type
 FROM raw_pokemon_reduced),
 
 type2table AS (SELECT
-pokedex_number, type2
+pokedex_number, type2 as type
 FROM raw_pokemon_reduced
 WHERE type2 IS NOT null)
 
